5e0177e3937a7b5e446c2aded7776b3b981c0f27
singhm26 <madhupsinghal321@gmail.com>
2024-09-25 23:49:39 +0530

    From

--- Controls/main.js
+++ Controls/main.js
@@ -1,4 +1,45 @@
-const button1 = document.getElementsByClassName("card-btns");
-console.log(button1);
-const button2 = document.getElementById("card-btns");
-console.log(button2);
+var contbtn = document.getElementById("contactBtn");
+var contform = document.getElementById("contactPopup");
+var closeBtn = document.getElementById("close");
+
+//Open the form with button
+contbtn.onclick = function () {
+  contform.style.display = "block";
+  //contform.style.position = "static";
+};
+
+//Closing the form
+closeBtn.onclick = function () {
+  contform.style.display = "none";
+};
+
+//handle submit button
+
+var response = document.getElementById("submitBtn");
+
+contform.onsubmit = function (event) {
+  event.preventDefault();
+};
+
+var formData = {
+  name: document.getElementById("name").value,
+  email: document.getElementById("email").value,
+  message: document.getElementById("message").value,
+};
+
+fetch("/submit", {
+  method: "POST",
+  headers: {
+    "content-Type": "application/json",
+  },
+  body: JSON.stringify(formData),
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("success", data);
+    alert("Your message has been sent!");
+    contform.style.display = "none";
+  })
+  .catch((error) => {
+    console.error("Error", error);
+  });
